f6a0e6d4281c34ab75665a5ee8cb8355a05c2752
ArkamEditz01 <arkamking005@gmail.com>
2026-08-26 09:09:15 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:example/home_page.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:video_trimmer/video_trimmer.dart';
+import 'trimmer_view.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,11 +10,54 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Video Trimmer',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Trimmer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Trimmer _trimmer = Trimmer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Video Trimmer"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("LOAD VIDEO"),
+          onPressed: () async {
+            FilePickerResult? result = await FilePicker.platform.pickFiles(
+              type: FileType.video,
+              allowCompression: false,
+            );
+            if (result != null && result.files.single.path != null) {
+              File file = File(result.files.single.path!);
+              await _trimmer.loadVideo(videoFile: file);
+              if (!mounted) return;
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) {
+                  return TrimmerView(_trimmer);
+                }),
+              );
+            }
+          },
         ),
-        home: const HomePage(),
-      );
+      ),
+    );
+  }
 }
