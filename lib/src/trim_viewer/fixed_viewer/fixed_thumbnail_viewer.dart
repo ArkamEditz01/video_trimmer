5f2ace9c06409baed345fa086b5b7c081e0512b8
ArkamEditz01 <arkamking005@gmail.com>
2026-09-19 09:36:16 +0530

    Update fixed_thumbnail_viewer.dart

--- lib/src/trim_viewer/fixed_viewer/fixed_thumbnail_viewer.dart
+++ lib/src/trim_viewer/fixed_viewer/fixed_thumbnail_viewer.dart
@@ -6,12 +6,14 @@ class FixedThumbnailViewer extends StatelessWidget {
   final Trimmer trimmer;
   final double appViewerHeight;
   final double appViewerWidth;
+  final File? videoFile;
 
   const FixedThumbnailViewer({
     super.key,
     required this.trimmer,
     required this.appViewerHeight,
     required this.appViewerWidth,
+    this.videoFile,
   });
 
   @override
@@ -24,11 +26,7 @@ class FixedThumbnailViewer extends StatelessWidget {
         borderRadius: BorderRadius.circular(4),
       ),
       child: const Center(
-        child: Icon(
-          Icons.movie_creation_outlined,
-          color: Colors.white38,
-          size: 24,
-        ),
+        child: Icon(Icons.movie_creation_outlined, color: Colors.white38, size: 24),
       ),
     );
   }
